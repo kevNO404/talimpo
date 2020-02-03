de853f19fbc48f35e948f8edd7ea6b6fc7a5d543
kevNO404 <48952176+kevNO404@users.noreply.github.com>
2020-02-03 20:42:09 -0300

    Update conexao.php

--- admPage/php/conexao.php
+++ admPage/php/conexao.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('ec2-54-174-221-35.compute-1.amazonaws.com','5432','dfff8bjgnk04ln','gvkldnxtmepliu','d63990eb22e68b442ae91a78c9b3bd2d924502b08b59c86957974dbd25f2132c') or die(mysqli_error()) ;
+$con = mysqli_connect('localhost','root','','tudolimpo') or die(mysqli_error()) ;
 
