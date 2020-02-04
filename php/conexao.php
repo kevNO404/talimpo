2d6abaffff965c4f71d471c1dbb093a61d4a896f
kevNO404 <48952176+kevNO404@users.noreply.github.com>
2020-02-03 21:38:53 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('us-cdbr-iron-east-04.cleardb.net','be63169f77567e','356dc7bf','heroku_2ff394e03900973') or die(mysqli_error()) ;
+$con = mysqli_connect('us-cdbr-iron-east-04.cleardb.net','be63169f77567e','3306','356dc7bf','heroku_2ff394e03900973') or die(mysqli_error()) ;
 
